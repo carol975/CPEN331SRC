c247b162bae5685c3f2e0ec63d6b870ec2b388b4
Carol Fu <fu.carol975@gmail.com>
2016-02-11 13:36:39 -0500

    added some notes

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -111,8 +111,9 @@ syscall(struct trapframe *tf)
 
 	    /* Add stuff here */
 	    case SYS_open:
-	    err = sys_open(
-
+	    err = sys_open((userptr_t)tf->tf_a0,(userptr_t)tf->tf_a1,(userptr_t)tf->tf_a2,(userptr_t)tf->tf_a3);
+        break;
+        
 	    default:
 		kprintf("Unknown syscall %d\n", callno);
 		err = ENOSYS;
