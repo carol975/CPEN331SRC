ed65a450ee4b682b816c390e7f65d17aabc5945d
Carol Fu <fu.carol975@gmail.com>
2016-02-13 02:12:06 -0500

    added file_syscall.h

---
+++ kern/include/file_syscall.h
@@ -0,0 +1,21 @@
+#ifndef _FILE_SYSCALL_H_
+#define _FILE_SYSCALL_H_
+
+#include <vfs.h>
+#include <vnode.h>
+#include <copyinout.h>
+#include <syscall.h>
+#include <kern/limits.h>
+#include <current.h>
+#include <proc.h>
+#include <uio.h>
+#include <kern/iovec.h>
+#include <filetable.h>
+#include <limits.h>
+
+int sys_open(const char *filename, int flags, mode_t mode);
+
+int sys_read(int fd, void *buf, size_t buflen);
+
+
+#endif
