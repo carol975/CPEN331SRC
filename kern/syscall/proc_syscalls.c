f7443bd08017ba5824000239c32dec7182c1fa74
Carol Fu <fu.carol975@gmail.com>
2016-03-15 16:39:33 -0700

    partially execv

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -24,29 +24,39 @@
 #include <mips/trapframe.h>
 #include <kern/wait.h>
 
+
 /* copy the current process */
 pid_t 
 sys_fork(struct trapframe *parent_tf, int *retval){
+	
+	if(parent_tf == NULL){
+		return EINVAL;
+	}
 
     int err = 0;
     
     /*Create the child process*/
 	 struct proc *child_proc = (struct proc *)kmalloc(sizeof(struct proc));
 	 err = proc_fork(&child_proc);  //the file handle is copied automatically
-	 //check//
+	 
+	 if(err){
+		 kfree(child_proc);
+		 return EMPROC;
+	 }
 	 
 	 /*Copy the parent addresspace to child addresspace	 */
 	err = as_copy(proc_getas(), &(child_proc->p_addrspace));
-	 if(err == ENOMEM){
+	 if(err){
 		 return ENOMEM;
 	 }
+	 
 	
 	/* Child trapframe is a copy of the parent trapframe */
 	struct trapframe *child_tf = (struct trapframe *)kmalloc(sizeof(struct trapframe));
 	*child_tf = *parent_tf;
 	
 	/* Create the child thread and attach to child_proc*/
-	//Should child_proc be replaced with NULL? since the process is switched
+
 	err = thread_fork("Child Thread", child_proc, &enter_forked_process,(void *)child_tf,0);
 	if(err){
 		kprintf("child thread_fork failed: %s\n",strerror(err));
@@ -79,27 +89,83 @@ sys_fork(struct trapframe *parent_tf, int *retval){
 /* execute a program */
 
 int 
-sys_execv(const char* program, char** args){
+sys_execv(const char* program, char** uargs){
+	
+	if(program == NULL ||uargs == NULL ){
+		return EFAULT;
+	}
+	
+	int err;
+	size_t actual;
+	
+	// Copy the program from user space to kernel space
+	char* k_program = (char*)kmalloc(sizeof(char) * PATH_MAX);
+	
+	err = copyinstr((const_userptr_t)program,k_program,PATH_MAX,&actual);
+	
+	if(err){
+		kfree(k_program);
+		return EFAULT;
+	}
+	
+	if(actual){
+		//empty program name
+		kfree(k_program);
+		return EINVAL;
+	}
+	
+	/*char **k_args = (char**)kmalloc(sizeof(char**));
+	err = copyin((const_userptr_t)uargs, k_args,sizeof(char*));
+	
+	if(err){
+		kfree(k_program);
+		kfree(k_args);
+		return EFAULT;
+	}
+	
+	*/
+	// Copy the user arguments into kernel arguments
+	int nargs = 0;
+	
+	while(uargs[nargs] != NULL){
+		++nargs;
+	}
+	
+	char ** k_args = (char **)kmalloc(sizeof(char) * nargs);
 
-	 struct addrspace *as;
+	int i;
+	
+	for(i = 0; i < nargs, ++i){
+		err = copyinstr((const_userptr_t)uargs[i], k_args[i],PATH_MAX,&actual);
+		if(err){
+			kfree(k_program);
+			kfree(k_args);
+			return EFAULT;
+		}
+	}
+	
+	++i;
+	args[i] = NULL;
+	
+	
+	
+	//========================================================================
+	struct addrspace *as;
 	struct vnode *v;
 	vaddr_t entrypoint, stackptr;
-	int err;
-	
-	if(args == NULL){
-	    return EFAULT;
-	}
-	/* open the executable, create a new address space and load the elf into it */
 	
 	/*Open the file */
 	err = vfs_open((char *)program, O_RDONLY,0,&v);
 	if(err){
+		kfree(k_program);
+		kfree(k_args);
 		return err;
 	}
 	
 	/* since execv is called after fork, we should be in the new process */
 	// since file table for child is already copied over from the parent, no need to
 	// create a new file table
+	
 	
 	// destroy the child_proc addrspace
 	if(curproc->p_addrspace != NULL){
@@ -110,6 +176,8 @@ sys_execv(const char* program, char** args){
 	// create a new addrspace for the child proc
 	as = as_create();
 	if(as == NULL){
+		kfree(k_program);
+		kfree(k_args);
 		vfs_close(v);
 		return ENOMEM;
 	}
@@ -117,12 +185,14 @@ sys_execv(const char* program, char** args){
 	// Switch to the newly created addresspace, that is, to completely abandon the addrspce it inherited from its parent
 	
 	proc_setas(as);
-	as_activate();
+	
 	
 	/* Load thee executable. */
 	err = load_elf(v, &entrypoint);
 	if(err){
 		/* p_addrspace will be destroyed when curproc is destroyed */
+		kfree(k_program);
+		kfree(k_args);
 		vfs_close(v);
 		return err; 
 	}
@@ -133,8 +203,55 @@ sys_execv(const char* program, char** args){
 	/* Define the user stack in the address space */
 	err = as_define_stack(as, &stackptr);
 	if(err){
+		kfree(k_program);
+		kfree(k_args);
 		return err;
 	}
+	
+	
+	
+	//==================================================================
+	//copy out the arguments
+	int arg_len;
+	int j = 0;
+	while(k_args[j] != NULL){
+		//get the length for 1 string of all strings in k_args
+		arg_len = strlen(k_args[j]) + 1; // extra space for null terminator
+		
+		//one string
+		char* arg_part;
+		
+		//allocate memory for the size of the current string in k_args
+		arg_part = kmalloc(sizeof(arg_len));
+		arg_part = kstrdup(k_args[j]);
+		
+		int index;
+		
+		//extract one string from k_args
+		for(index = 0; index < arg_len; ++index){
+			arg_part[index] = k_args[j][index];
+		}
+		
+		//decrease the stack pointer by one memory block, i.e. move to the next string
+		stackptr -= arg_len;
+		
+		err = copyout((const void *)arg_part, (userptr_t)stackptr, (size_t)arg_len);
+		if(err){
+			kfree(k_program);
+			kfree(k_args);
+			kfree(arg_part);
+			return err;
+		}
+		
+		kfree(arg_part);
+		
+		k_args[j] = (char *)stackptr;
+		
+		for(in)
+	}
+	
+	
+	
 	
 	/* Go back to user mode */
 	/* Note: argc should be tf_a0, argv should be tf_a1l
@@ -144,7 +261,7 @@ sys_execv(const char* program, char** args){
 	/*enter_new_process should not return */
 	panic("enter_new_process returned\n");
 	return EINVAL;
-
+	
 
 }
 
